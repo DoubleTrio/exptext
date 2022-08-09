00da65e99c98e6bfbfa94089508d0e4d7360041e
Kacey La <lakacey03@gmail.com>
2022-08-09 19:42:40 -0400

    add multiple files test

--- tests/cli.rs
+++ tests/cli.rs
@@ -14,6 +14,21 @@ const M04: &str = "tests/inputs/m04a0101.txt";
 const M12: &str = "tests/inputs/m12a0102.txt";
 const ALL: [&str; 4] = [M00, M01, M04, M12];
 
+fn init_commands() -> [CommandOptions<'static>; 10] {
+    [
+        CommandOptions::new(&[], "", "No command options"),
+        CommandOptions::new(&["-f"], ".f", "Should not format text"),
+        CommandOptions::new(&["-n"], ".n", "Should number lines"),
+        CommandOptions::new(&["-s"], ".s", "Should sort lines"),
+        CommandOptions::new(&["-fn"], ".fn", "No format and number lines"),
+        CommandOptions::new(&["-fs"], ".fs", "No format and sort"),
+        CommandOptions::new(&["-ns"], ".ns", "Line number and sort"),
+        CommandOptions::new(&["-l", "e"], ".l-e", "Filter by english"),
+        CommandOptions::new(&["-nl", "e"], ".nl-e", "Filter by english and number lines"),
+        CommandOptions::new(&["-fnsl", "e"], ".fnsl-e", "Combine all options"),
+    ]
+}
+
 struct CommandOptions<'a> {
     pub options: &'a [&'a str],
     pub file_end: &'a str,
@@ -31,10 +46,10 @@ impl<'a> CommandOptions<'a> {
     }
 }
 
-fn run(args: &[&str], input_file: &str, expected_file: &str) -> TestResult {
+fn run(args: &[&str], input_files: &[&str], expected_file: &str) -> TestResult {
     let expected = fs::read_to_string(expected_file)?;
     Command::cargo_bin(PRG)?
-        .args([DB, DB_NAME, input_file].iter().chain(args))
+        .args([DB, DB_NAME].iter().chain(input_files).chain(args))
         .assert()
         .success()
         .stdout(expected);
@@ -42,20 +57,8 @@ fn run(args: &[&str], input_file: &str, expected_file: &str) -> TestResult {
 }
 
 #[test]
-fn all() -> TestResult {
-    let commands: [CommandOptions; 10] = [
-        CommandOptions::new(&[], "", "No command options"),
-        CommandOptions::new(&["-f"], ".f", "Should not format text"),
-        CommandOptions::new(&["-n"], ".n", "Should number lines"),
-        CommandOptions::new(&["-s"], ".s", "Should sort lines"),
-        CommandOptions::new(&["-fn"], ".fn", "No format and number lines"),
-        CommandOptions::new(&["-fs"], ".fs", "No format and sort"),
-        CommandOptions::new(&["-ns"], ".ns", "Line number and sort"),
-        CommandOptions::new(&["-l", "e"], ".l-e", "Filter by english"),
-        CommandOptions::new(&["-nl", "e"], ".nl-e", "Filter by english and number lines"),
-        CommandOptions::new(&["-fnsl", "e"], ".fnsl-e", "Combine all options"),
-    ];
-
+fn handles_single_file() -> TestResult {
+    let commands = init_commands();
     for command_op in commands {
         let CommandOptions {
             options,
@@ -66,7 +69,7 @@ fn all() -> TestResult {
             let path = Path::new(file);
             match run(
                 options,
-                file,
+                &[file],
                 format!(
                     "tests/expected/{}{}.out",
                     path.file_name().unwrap().to_str().unwrap(),
@@ -77,6 +80,27 @@ fn all() -> TestResult {
                 Ok(_) => continue,
                 Err(e) => eprintln!("{}", e),
             }
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn handles_multiple_files() -> TestResult {
+    let commands = init_commands();
+    for command_op in commands {
+        let CommandOptions {
+            options,
+            file_end,
+            description: _,
+        } = command_op;
+        match run(
+            options,
+            &ALL,
+            format!("tests/expected/all{}.out", file_end).as_str(),
+        ) {
+            Ok(_) => continue,
+            Err(e) => eprintln!("{}", e),
         }
     }
     Ok(())
